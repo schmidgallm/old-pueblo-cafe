9714afa5553b4c07ff763b56b1b980b3c1690dba
schmidgallm <schmidgallm@outlook.com>
2020-06-24 14:56:15 -0700

    add events page ui

--- src/components/Pages/Events/Events.js
+++ src/components/Pages/Events/Events.js
@@ -5,6 +5,37 @@ import React from 'react';
 import './Events.css';
 
 const Events = () => {
-  return <div>Events</div>;
+  return (
+    <div className='events-page container'>
+      <h1>Have an Event? We have plenty of space!</h1>
+      <div className='cards'>
+        <div className='card'>
+          <h5 class='card-header'>Private Parties</h5>
+          <div class='card-body'>
+            <p class='card-text'>
+              We can accommodate large groups up to 150 people at our
+              restaurant, typically on our covered patio. Give us a call today
+              to learn more about hosting your next event with us!
+            </p>
+          </div>
+        </div>
+        <div className='card'>
+          <h5 class='card-header'>Catering</h5>
+          <div class='card-body'>
+            <p class='card-text'>
+              Available for pick-up or delivery. Equipment, such as chafing
+              dishes, available for rent.
+            </p>
+          </div>
+        </div>
+        <div className='card'>
+          <h5 class='card-header'>Reservations </h5>
+          <div class='card-body'>
+            <p class='card-text'>Available for parties of 8 or more only.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Events;
