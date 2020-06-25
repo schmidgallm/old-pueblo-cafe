47df8183594031eb48097c6402b1c240f095e4b3
schmidgallm <schmidgallm@outlook.com>
2020-06-25 09:18:01 -0700

    add menu page with useState and useEffect to load which menu

--- src/components/Pages/Menu/Menu.js
+++ src/components/Pages/Menu/Menu.js
@@ -1,10 +1,53 @@
 // Dependencies
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 // Import Menu CSS
 import './Menu.css';
 
 const Menu = () => {
-  return <div>Menu</div>;
+  // set state for which menu option to choose
+  const [menu, setMenu] = useState('');
+
+  useEffect(() => setMenu('dinner'), []);
+
+  return (
+    <div className='menu-page container'>
+      <div className='menu-header'>
+        <h1>Menu</h1>
+        <p className='menu-icon'>
+          <span
+            style={
+              menu === 'dinner'
+                ? { color: '#fff', backgroundColor: '#85d8d6', padding: '5px' }
+                : null
+            }
+            onClick={() => setMenu('dinner')}
+          >
+            Dinner
+          </span>{' '}
+          |{' '}
+          <span
+            style={
+              menu === 'lunch'
+                ? { color: '#fff', backgroundColor: '#85d8d6', padding: '5px' }
+                : null
+            }
+            onClick={() => setMenu('lunch')}
+          >
+            Lunch
+          </span>
+        </p>
+      </div>
+      <div className='menu-images'>
+        <div className='menu-img'>
+          {menu === 'dinner' ? (
+            <img src='/static/img/dinnerMenu.png' alt='dinner menu' />
+          ) : (
+            <img src='/static/img/lunchMenu.png' alt='lunch menu' />
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Menu;
